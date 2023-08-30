169115dd073d888f3119c0dcd6366787b68400c3
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-08-30 17:36:43 +0100

    Update shaderpkg.bat

--- shaderpkg.bat
+++ shaderpkg.bat
@@ -1,3 +1,3 @@
 @echo off
-"./tools/shader_minifier.exe" -o compressed.glsl --format text --smoothstep --aggressive-inlining %1
+"./tools/shader_minifier.exe" -o minified.glsl --format text --smoothstep --aggressive-inlining %1
 "./src/ShaderCompressor/bin/x64/Release/ShaderCompressor.exe" minified.glsl compressed.txt
